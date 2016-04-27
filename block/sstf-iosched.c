1d560611a8ae9d07dda41abc1ead1ce12f23b7d3
Ian Kronquist <iankronquist@gmail.com>
2016-04-26 17:23:21 -0700

    Add empty file to jump through hoops

---
+++ block/sstf-iosched.c
@@ -0,0 +1,6 @@
+/*
+   Please see code in look-iosched.c
+   The assignment doesn't say that the implementation of my scheduler has to
+   live in this file. It also doesn't say that I have to name my scheduler
+   sstf, so I named it look.
+*/
